0225c97dd8d59d8298bba47842aaa3aa5757a6b7
MohamedSalem <engsalem500@gmail.com>
2025-06-14 21:09:48 +0300

    App_Sections

---
+++ lib/features/app_sections/GymPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GymPage extends StatefulWidget {
+  const GymPage({super.key});
+
+  @override
+  State<GymPage> createState() => _GymPageState();
+}
+
+class _GymPageState extends State<GymPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("Gym Page",style: TextStyle(color: Colors.white, fontSize: 24)),);
+  }
+}
