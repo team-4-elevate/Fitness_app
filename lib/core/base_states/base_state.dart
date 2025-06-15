76cecefca62fa580150af9419b850a7d6468db75
Loay Elhamaky <loay.elhamaky@gmail.com>
2025-06-15 22:55:32 +0300

    fix base state

--- lib/core/base_states/base_state.dart
+++ lib/core/base_states/base_state.dart
@@ -4,30 +4,30 @@ sealed class BaseState<T> extends Equatable {
   const BaseState();
 }
 
-final class InitialState<T> extends BaseState<T> {
-  const InitialState();
+final class BaseInitialState<T> extends BaseState<T> {
+  const BaseInitialState();
 
   @override
   List<Object?> get props => [];
 }
 
-final class LoadingState<T> extends BaseState<T> {
-  const LoadingState();
+final class BaseLoadingState<T> extends BaseState<T> {
+  const BaseLoadingState();
 
   @override
   List<Object?> get props => [];
 }
 
-final class SuccessState<T> extends BaseState<T> {
-  const SuccessState(this.data);
-  final T data;
+final class BaseSuccessState<T> extends BaseState<T> {
+  const BaseSuccessState({this.data});
+  final T? data;
 
   @override
   List<Object?> get props => [data];
 }
 
-final class ErrorState<T> extends BaseState<T> {
-  const ErrorState(this.error);
+final class BaseErrorState<T> extends BaseState<T> {
+  const BaseErrorState(this.error);
   final String error;
 
   @override
