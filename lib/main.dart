83fdb64e429ec1ee2f287e5ef29b5ccd8a7fab94
GitHub Action <action@github.com>
2025-06-20 23:22:52 +0000

    style: apply dart format changes

--- lib/main.dart
+++ lib/main.dart
@@ -76,19 +76,21 @@ class _FitnessAppState extends State<FitnessApp> {
 
   @override
   Widget build(BuildContext context) {
-     final appBloc = getIt<AppBloc>();
+    final appBloc = getIt<AppBloc>();
     return BlocProvider.value(
       value: getIt<AppBloc>(),
       child: BlocListener<AppBloc, AppState>(
-        listenWhen: (previous, current) => previous.appLocale != current.appLocale,
+        listenWhen:
+            (previous, current) => previous.appLocale != current.appLocale,
         listener: (context, state) {
           getIt<LocalizationManager>().setLocale(Locale(state.appLocale));
-          setState(() {}); 
+          setState(() {});
         },
         child: BlocBuilder<AppBloc, AppState>(
-          buildWhen: (previous, current) => 
-            previous.isLoggedIn != current.isLoggedIn || 
-            previous.isShowOnboarding != current.isShowOnboarding,
+          buildWhen:
+              (previous, current) =>
+                  previous.isLoggedIn != current.isLoggedIn ||
+                  previous.isShowOnboarding != current.isShowOnboarding,
           builder: (context, state) {
             return ResponsiveWrapper(
               child: MaterialApp(
@@ -110,13 +112,16 @@ class _FitnessAppState extends State<FitnessApp> {
                   ApiLocalizationService().setLocalizations(localizations);
                   return child!;
                 },
-                initialRoute: !state.isShowOnboarding
-                    ? AppRoutes.onboarding
-                    : (appBloc.state.isLoggedIn ? AppRoutes.layoutScreen : AppRoutes.loginPage),
+                initialRoute:
+                    !state.isShowOnboarding
+                        ? AppRoutes.onboarding
+                        : (appBloc.state.isLoggedIn
+                            ? AppRoutes.layoutScreen
+                            : AppRoutes.loginPage),
               ),
             );
-            },
-          ),
+          },
+        ),
       ),
     );
   }
